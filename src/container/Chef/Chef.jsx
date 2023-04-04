3b0428924b59faedbc2d4781b8f7487bd48778de
Shawlycode <kutinaorlando@gmail.com>
2023-04-04 18:24:02 +0000

    added intro video

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import { images } from '../../constants'
+import { SubHeading } from '../../components';
 
 import './Chef.css';
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className='app__bg app__wrapper section__padding'>
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef" />
+    </div>
+    <div className="app__wrapper_info">
+      <SubHeading title={'Chefs Word'} />
+      <h1 className='headtext__cormorant'>What We Believe In</h1>
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote" />
+          <p className="p__opensans"> Lorem ipsum dolor sit am adipisicing elit. Et, exercitationem. </p>
+        </div>
+        <p className='p__opensans'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, alias? Ducimus dolorem dicta, sunt itaque assumenda, repellendus repellat suscipit eius incidunt earum esse tenetur, dolores illum? Pariatur quod consectetur laudantium!.</p>
+      </div>
+      <div className="app__chef-sign">
+        <p>Kelvin Luo</p>
+        <p className="p__opensans"> Chef & Founder</p>
+        <img src={images.sign} alt="chef sign" />
+      </div>
+    </div>
   </div>
 );
 
--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 
+import { meal } from '../../constants'
 import './Intro.css';
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+const Intro = () => {
 
+
+  const [isPlaying, setIsPlaying] = useState(false);
+  const videoRef = useRef(null);
+
+  const togglePlay = () => {
+    if (isPlaying) {
+      videoRef.current.pause();
+    } else {
+      videoRef.current.play();
+    }
+    setIsPlaying(!isPlaying);
+  };
+
+  return (
+    <div className='app__video video__wrapper'>
+      <video
+        ref={videoRef}
+        width="100%"
+        height="100%"
+        controls={false}
+        src={meal}
+        bigPlayButtonCentered={true}
+      >
+
+
+      </video>
+      <div className="video__overlay">
+        <button onClick={togglePlay} >
+          {isPlaying ? "Pause" : "Play"}
+        </button>
+      </div>
+    </div>
+  )
+};
 export default Intro;
