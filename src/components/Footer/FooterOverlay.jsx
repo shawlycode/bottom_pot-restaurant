8ffd461a1ead5d17a53743f43779f8841c1a59ea
Shawlycode <kutinaorlando@gmail.com>
2023-04-08 13:51:30 +0000

    footer committed

--- src/components/Footer/FooterOverlay.jsx
+++ src/components/Footer/FooterOverlay.jsx
@@ -6,37 +6,39 @@ import './FooterOverlay.css';
 
 const FooterOverlay = () => (
   <div className='app__bg app__footerOverlay' >
-    <div className="app__footer_content" >
-      <p className="p__opensans" >Contact Us</p>
-      <p className="p__opensans">Contact & Locate us now </p>
-      <p className="p__opensans">Lapaz - Blk 45A, Nyormo Street </p>
-      <p className="p__opensans">+233- 0200-9471-290 </p>
-      <p className="p__opensans">C233- 0200-9471-020 </p>
-
-    </div>
-    <div className="app__footer_img">
-      <div className="footer__img">
-        <img src={images.gericht} alt="logo" />
-      </div>
-      <p className="p__opensans">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, vero?</p>
-      <SubHeading />
-      <div className="app__footer_icons">
-        <RiFacebookFill className='icons' />
-        <RiInstagramLine className='icons' />
-        <RiTwitterFill className='icons' />
-
+    <div className="app__footerOverlay-content">
+      <div className="app__footer_content" >
+        <p className="p__opensans" >Contact Us</p>
+        <p className="p__opensans">Contact & Locate us now </p>
+        <p className="p__opensans">Lapaz - Blk 45A, Nyormo Street </p>
+        <p className="p__opensans">+233- 0200-9471-290 </p>
+        <p className="p__opensans">C233- 0200-9471-020 </p>
 
       </div>
-    </div>
-    <div className="app__footer_content">
-      <p className="p__opensans">Working Hours</p>
-      <p className="p__opensans">Mon - Fri</p>
-      <p className="p__opensans"> 10:00am - 2:00am</p>
+      <div className="app__footer_img">
+        <div className="footer__img">
+          <img src={images.gericht} alt="logo" />
+        </div>
+        <p className="p__opensans">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, vero?</p>
+        <SubHeading />
+        <div className="app__footer_icons">
+          <RiFacebookFill className='icons' />
+          <RiInstagramLine className='icons' />
+          <RiTwitterFill className='icons' />
 
-      <p className="p__opensans">Sun - Sat</p>
-      <p className="p__opensans">10:00am - 3:00am</p>
-    </div>
 
+        </div>
+      </div>
+      <div className="app__footer_content">
+        <p className="p__opensans">Working Hours</p>
+        <p className="p__opensans">Mon - Fri</p>
+        <p className="p__opensans"> 10:00am - 2:00am</p>
+
+        <p className="p__opensans">Sun - Sat</p>
+        <p className="p__opensans">10:00am - 3:00am</p>
+      </div>
+    </div>
+    <div className="app__footer_copyRight"><p className="p__opensans">All Rights Reserve | &copy;2023 </p></div>
   </div>
 );
 
--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -8,10 +8,12 @@ const Newsletter = () => (
     <SubHeading title="News Letter" />
     <h1 className="headtext__cormorant">Subscribe to  our newsletter</h1>
     <p className="p__opensans">And never miss our menu updates </p>
-    <div className="app__neswletter_input">
+    <div className="app__newsletter_input">
       <input type="text" placeholder='enter your email' />
       <button type='button' className='custom__button'>Subscribe</button>
+
     </div>
+
   </div >
 );
 
