e0af382b3444f6e52b25ab2369415ca94d9f5ff3
Shawlycode <kutinaorlando@gmail.com>
2023-04-08 11:29:52 +0000

    newsletter commit

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import SubHeading from '../SubHeading/SubHeading';
 
 import './Newsletter.css';
 
 const Newsletter = () => (
-  <div>
-    Newsletter
-  </div>
+  <div className='app__bg app__newsLetter' >
+    <SubHeading title="News Letter" />
+    <h1 className="headtext__cormorant">Subscribe to  our newsletter</h1>
+    <p className="p__opensans">And never miss our menu updates </p>
+    <div className="app__neswletter_input">
+      <input type="text" placeholder='enter your email' />
+      <button type='button' className='custom__button'>Subscribe</button>
+    </div>
+  </div >
 );
 
 export default Newsletter;
